8a27b5de823cbe70ee85784184c8f0915b24f80b
rowley1 <120111298+rowley1@users.noreply.github.com>
2023-01-05 00:55:01 +0300

    Update Header.jsx

--- components/Static/Header.jsx
+++ components/Static/Header.jsx
@@ -76,7 +76,7 @@ const Header = ({ $, NavItems }) => {
                 <div className="max-w-7xl px-5 mx-auto py-5 flex items-center justify-between">
                     <div className="flex items-center space-x-6">
                         <div className="flex items-center space-x-3">
-                            <img src="/img/logo2.png" 
+                            <img src="https://cdn.discordapp.com/attachments/1006962822363750401/1060312614892220416/Junibot_Community.webp" 
                                 className={`
                                     rounded-full 
                                 `}
