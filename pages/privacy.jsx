2552e3ae41b8b637d15148913824ae8ed72b7c4f
rowley1 <120111298+rowley1@users.noreply.github.com>
2023-01-04 22:22:11 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -3,7 +3,7 @@ export default function Privacy() {
     return (
         <div>
             <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Junibot</h1>
-            <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
+            <p className="text-sm text-white/30 mb-4">Last Update: 04/01/2023  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
  
 <p><a className="text-white">DATA COLLECTİON/STORAGE</a></p>
