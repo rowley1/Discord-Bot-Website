6cd09645fc9fe4e2f98d3e5b45daed9a0fd0c693
rowley1 <120111298+rowley1@users.noreply.github.com>
2023-01-05 00:29:12 +0300

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -6,25 +6,36 @@ export default function Tos() {
   <div className="animateHeader text-white text-opacity-40 mt-5">
 
 <p>These Terms of Service ("Terms") govern your usage of any Services provided by Junibot ("us", "we", "our"). Please read these terms carefully.</p>
-    
-<p>Accepting these Terms
-    
-    
-    
-    
-    
-<p>⊳ Every discord server can use the Astra bot for free, it is completely prohibited to sell or market for a fee.</p>
 <div className="py-3"></div>
-<p>⊳ You cannot use bot commands to harm the bot.</p>
+  
+<p><a className="text-white">ACCEPTİNG THESE TERMS</a></p>
+<p>By accessing or using our Services, you agree to abide by these Terms and our <a className="text-white" href="https://junibot.vercel.app/privacy">Privacy Policy</a>.You are also agreeing that you have accepted any terms governing your access to Discord (discord.com) as the platform our Services reside on and/or are for, you may view these at <a className="text-white" href="https://discord.com/terms">https://discord.com/terms</a> and <a className="text-white" href="https://discord.com/guidelines">https://discord.com/guidelines</a>.    
+If you do not, or no longer, agree to any of these Terms, you may not access or use our Services.</p>
 <div className="py-3"></div>
-<p>⊳ You may not use open source codes to harm other servers.</p>
+  
+<p><a className="text-white">TERMİNATİON</a></p>  
+<p>You may stop accessing or using our Services at any time without prior notice.
+
+We may terminate or suspend your access or use of our Services at any time, without any prior notice, for any reason within or beyond these Terms.</p> 
 <div className="py-3"></div>
-<p>⊳ You may not sell or market open source codes through other platforms.</p>
+  
+<p><a className="text-white">INDEMNİFİCATİON</a></p>
+<p>You will defend, indemnify and hold Junibot and its Staff harmless against all liablities, damages, losses, costs and fees (including legal fees) in any way relating to (i) your access to or use of our Services, (ii) your actual or alleged breach of these Terms.</p>
+<div className="py-3"></div> 
+  
+<p><a className="text-white">DİSCLAİMER</a></p>
+<p>Your access to or use of our Services is at your sole risk. Our Services are provided on an "AS IS" basis. The Service is provided without warranties of any kind, whether express or implied, including, but not limited to, implied warranties of merchantability, fitness for a particular purpose, non-infringement or course of performance.
+  
+You understand and agree that you could be exposed to content that may be inaccurate, objectionable, inappropriate for children or otherwise unsuited to your purpose. Some users of our Services will willing choose to disregard our Terms and misuse our Services. If you see any content like this, please report it to us at our contact below so we can handle it appropriately.</p>   
 <div className="py-3"></div>
-<p>⊳ While using the bot, you cannot use it in a way that harms other users for troll purposes.</p>
+  
+<p><a className="text-white">CHANGES TO THESE TERMS</a></p>
+<p>We reserve the right to update or modify these Terms at any time and this could be done without prior notice. We recommend reviewing our Terms periodically. The last time these Terms were updated is shown at the top of the page. Continuing to access or use our Services after any changes have been made will indicate your agreement to the revised Terms.</p>
 <div className="py-3"></div>
-<p>⊳ To use certain discord bot commands, you need to vote for the <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">Astra</a> bot at certain intervals.</p>
-</div>
+  
+<p><a className="text-white">CONTACT US</a></p>
+<p>If you have any questions or concerns, you may contact us on our <a className="text-white" href="https://discord.gg/zG5CDR4sxH">Junibot Support Server.</a></p>
+ </div>
     </div>
   );
 }
