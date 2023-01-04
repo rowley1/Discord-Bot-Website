533c4123c3125a5ff3c8f32a75d6b13bbca45daa
rowley1 <120111298+rowley1@users.noreply.github.com>
2023-01-04 22:21:28 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -9,28 +9,28 @@ export default function Privacy() {
 <p><a className="text-white">DATA COLLECTİON/STORAGE</a></p>
 <p>Data collected and stored <a className="text-white">(when necessary)</a> is done when provided by a user voluntarily via running the bot's commands.
 Very minimal data is collected <a className="text-white">(Discord user ID)</a> automatically for moderation purposes to provide our service.</p>
-<div className="py-3"></div>
+
 <p><a className="text-white">THİRD PARTİES</a></p>
 <p>We do not share any data with third parties.</p>
-<div className="py-3"></div>
+
 <p><a className="text-white">STORAGE DURATİON</a></p>
 <p>Message data can be stored per server when a server enables the bot's logging features. This storage is temporary and dependant on bot uptime. This data will be deleted immediately when a message is deleted.
 Other data may be stored indefinitely <a className="text-white">(unless deletion is requested)</a> to allow for ease of reuse if a server removes the bot and readds it at a future date.</p>
-<div className="py-3"></div>
+
 <p><a className="text-white">USER RESPONSİBİLİTY</a></p>
 <p>• Users are responsible for the data they provide to our bot.
 • If you do not agree to any data being collected it is advised to not use our bot.
 • Data may still be collected for moderation purposes from servers using our bot.</p>
-<div className="py-3"></div>
+
 <p><a className="text-white">DATA DELETİON</a></p>
 <p>You may request to have certain data removed.
 For individual users, you may request to have data related to you deleted <a className="text-white">(such as reminder data)</a> at our contact below <a className="text-white">(not including deletion of data used by servers, such as moderation data)</a>.
 For servers, a server owner or other representative of the server <a className="text-white">(holding a position with Administrator permissions)</a> may also contact us below to have the information held for your server deleted entirely. Note that this will likely affect the functionality of the bot on your server unless reconfigured.
 Requests for deletion of data could take up to <a className="text-white">30 Days</a> to be fulfilled.</p>
-<div className="py-3"></div>
+
 <p><a className="text-white">CHANGES TO THİS POLİCY</a></p>
 <p>We reserve the right to update or modify this Policy at any time and this could be done without prior notice. We recommend reviewing our Policy periodically. The last time this Policy was updated is shown at the top of the page. Continuing to use our bot after any changes have been made will indicate your agreement to the revised Policy.</p>    
-<div className="py-3"></div>    
+    
 <p><a className="text-white">CONTACT US</a></p>    
 <p>You may contact us via our <a className="text-white" href="https://discord.gg/zG5CDR4sxH">Junibot Support Server</a>.
       </div>
